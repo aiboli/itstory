fc1563be5866fec51e016de2ffcc2c11c638ad7f
aiboli <albertlabtech@gmail.com>
2019-05-02 23:49:23 -0700

    change text

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'I love you Danli baby' });
+  res.render('index', { title: 'Itstory' });
 });
 
 module.exports = router;
